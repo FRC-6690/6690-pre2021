0bc483ac9cc545a70c1be3b74152384c66f48a54
PrudentPluto431 <73255601+PrudentPluto431@users.noreply.github.com>
2021-01-14 16:23:19 -0500

    Add CAN Id's to constants
    added CAN id's and all drive train motors

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,4 +12,12 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+    public static final class CANIds{
+        public static final int frontStarboardDriveMotor = 0;   //configured never
+        public static final int frontPortDriveMotor = 1;        // configured never
+        public static final int backStarboardDriveMotor = 2;    // configured never   
+        public static final int backPortDriveMotor = 3;         // configured never
+
+    }
+}
